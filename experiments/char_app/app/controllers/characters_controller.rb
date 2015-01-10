6f386b6c19684c1bcb2fe3d810b71e7ee1ca4be8
Greg Althaus <galthaus@austin.rr.com>
2015-01-10 14:36:51 -0600

    Add point to character sheet at end of description.

--- experiments/char_app/app/controllers/characters_controller.rb
+++ experiments/char_app/app/controllers/characters_controller.rb
@@ -12,7 +12,8 @@ class CharactersController < ApplicationController
 
   def show
     j = JSON.parse(obsidian_portal.access_token.get('/v1/users/me.json').body)
-    @char = JSON.parse(obsidian_portal.access_token.get("/v1/campaigns/#{j['campaigns'][0]['id']}/characters/#{params[:id]}.json").body)
+    char_url = "/v1/campaigns/#{j['campaigns'][0]['id']}/characters/#{params[:id]}.json"
+    @char = JSON.parse(obsidian_portal.access_token.get(char_url).body)
 
     respond_to do |format|
         format.html { render layout: "character_view" }
@@ -21,6 +22,22 @@ class CharactersController < ApplicationController
           wiki_all_url = "/v1/campaigns/#{camp_id}/wikis.json"
 
 #          Rails.logger.fatal(JSON.pretty_generate(@char));
+
+          # Check to see if char has a description url, if not add one.
+          if !@char["description"].include? "View Character Sheet"
+            new_description = @char["description"]
+            new_description += "\n\"View Character Sheet\":http://70.113.106.34/character/#{params[:id]}.json"
+            d_data = {
+              'character' => {
+                'description' : new_description
+              }
+            }
+
+            resp = obsidian_portal.access_token.put(char_url, d_data.to_json,
+                                             {'Content-Type' => 'application/x-www-form-urlencoded'})
+            Rails.logger.fatal("GREG: response to update description: #{resp.inspect}")
+            Rails.logger.fatal("GREG: response to update description: #{resp.body}")
+          end
 
           all_wikis = JSON.parse(obsidian_portal.access_token.get(wiki_all_url).body)
 
