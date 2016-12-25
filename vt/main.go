fbd829b9a403eea7f65339a449a86ad138e02447
Greg Althaus <galthaus@austin.rr.com>
2016-12-25 11:29:45 -0600

    Add files and templates.

--- vt/main.go
+++ vt/main.go
@@ -1,11 +1,14 @@
 package main
 
-import "gopkg.in/gin-gonic/gin.v1"
+import (
+	"gopkg.in/gin-gonic/gin.v1"
+	"net/http"
+)
 
 func main() {
 	r := gin.Default()
 
-	r.StaticFS("/villian_throwdown", http.Dir("villain_throwdown"))
+	r.StaticFS("/villian_throwdown", http.Dir("villian_throwdown"))
 	r.LoadHTMLGlob("templates/*")
 	r.GET("/index", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
